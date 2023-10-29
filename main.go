af62350af9245fd86bf50cdc8aaec64f68e550c2
shgzhou <145970375+bertonzhou@users.noreply.github.com>
2023-10-29 20:41:12 +0800

    集成 gin

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello 世界")
+
+	// new 一个 Gin Engine 实例
+	r := gin.New()
+
+	// 注册中间件
+	r.Use(gin.Logger(), gin.Recovery())
+
+	// 注册一个路由
+	r.GET("/", func(c *gin.Context) {
+
+		// 以 JSON 格式响应
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+
+	// 运行服务，默认为 8080，我们指定端口为 8000
+	r.Run(":8000")
 }
