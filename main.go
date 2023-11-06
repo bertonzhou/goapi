490852662e8e298da6ddcd539fce74e108bf9e08
shgzhou <berton2023925@proton.me>
2023-11-06 16:17:13 +0800

    HTTP 访问日志

--- main.go
+++ main.go
@@ -26,6 +26,12 @@ func main() {
 	// 初始化 Logger
 	bootstrap.SetupLogger()
 
+	// 设置 gin 的运行模式，支持 debug, release, test
+	// release 会屏蔽调试信息，官方建议生产环境中使用
+	// 非 release 模式 gin 终端打印太多信息，干扰到我们程序中的 Log
+	// 故此设置为 release，有特殊情况手动改为 debug 即可
+	gin.SetMode(gin.ReleaseMode)
+
 	// new 一个 Gin Engine 实例
 	router := gin.New()
 
@@ -41,4 +47,7 @@ func main() {
 		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
 	}
+
+	// 打印访问地址
+	fmt.Println("Server run at: http://127.0.0.1:" + config.Get("app.port"))
 }
